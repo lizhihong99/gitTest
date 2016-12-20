4d9b13cfa02765c2e8552c597f5fd5a3f4fa3d68
lizhihong99 <lizhihong_job@sina.com>
2016-12-20 21:08:25 +0800

    Update Test01.java

--- src/com/test/Test01.java
+++ src/com/test/Test01.java
@@ -1,5 +1,9 @@
 package com.test;
 
 public class Test01 {
-
+   public static void main(String[] args) {
+		System.out.println("1111111111111");
+     System.out.println("1111111111111");
+     System.out.println("1111111111111");
+	}
 }
